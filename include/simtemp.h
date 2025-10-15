810d7ff422605272bad6237b1da0ea792b0f6ded
fideuardo <fideuardo@gmail.com>
2025-10-15 09:41:04 -0600

    the prefix nxp_ was added according withe MSD

--- include/simtemp.h
+++ include/simtemp.h
@@ -1,5 +1,5 @@
-#ifndef _SIMTEMP_H_
-#define _SIMTEMP_H_
+#ifndef _NXP_SIMTEMP_H_
+#define _NXP_SIMTEMP_H_
 /*types*/
 #include <linux/types.h>
 
@@ -9,28 +9,28 @@
 #include <linux/miscdevice.h>
 
 /*Own libraries*/
-#include <simtemp_ringbuf.h>
+#include <nxp_simtemp_ringbuf.h>
 #include <uapi/simtemp_uapi.h>
 
 /*Operation Modes */
-enum simtemp_mode {
+enum nxp_simtemp_mode {
 	SIMTEMP_MODE_ONESHOT = 0,
     SIMTEMP_MODE_CONTINUOUS,
 };
 
 /* Driver States */
-enum simtemp_state {
+enum nxp_simtemp_state {
 	SIMTEMP_enSTATE_STOP = 0,
 	SIMTEMP_enSTATE_RUN,
 	SIMTEMP_STATE_ERROR, /*Pending to add more states*/
 };
 
 /*Device Context */
-struct simtemp_dev {
+struct nxp_simtemp_dev {
     /*kernel section */
     struct device *dev;
     /*Control elements*/
-    enum simtemp_state state; /*Driver State: STOPPED, RUNNING, ERROR*/
+    enum nxp_simtemp_state state; /*Driver State: STOPPED, RUNNING, ERROR*/
 
     wait_queue_head_t read_wait; /* Wait queue for polling */
     /* high resolution timer for periodic sampling */
@@ -40,14 +40,14 @@ struct simtemp_dev {
     /* sequence counter for samples */
     u64 u64SequenceNumber;
     /*user section */
-    enum simtemp_mode mode;
+    enum nxp_simtemp_mode mode;
     /*data section*/
-    struct simtemp_ringbuffer stRingBuff; /* Ring buffer instance */
+    struct nxp_simtemp_ringbuffer stRingBuff; /* Ring buffer instance */
     
 };
 
 /* --- Funciones exportadas desde otros archivos --- */
-int simtemp_of_parse(struct device *dev, struct simtemp_dev *sdev);
+int nxp_simtemp_of_parse(struct device *dev, struct nxp_simtemp_dev *sdev);
 
 
-#endif /* _SIMTEMP_H_ */
+#endif /* _NXP_SIMTEMP_H_ */
